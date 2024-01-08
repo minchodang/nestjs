cbad99564bf49152349c52f07fc2f606237a07a8
minchodang <minsu910725@gmail.com>
2024-01-08 22:31:42 +0900

    Feat: prisma-orm, todo api 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodosController } from './todos/todos.controller';
 import { PostsModule } from './posts/posts.module';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [PostsModule],
+  imports: [PostsModule, TodoModule],
   controllers: [AppController, TodosController],
   providers: [AppService],
 })
